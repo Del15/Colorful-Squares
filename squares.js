06b82aefef37733c1973bea43211c65842e96347
Del15 <ellisderrick205@gmail.com>
2025-11-12 11:19:13 -0500

    added squares that are a diffent color from the previous one

--- squares.js
+++ squares.js
@@ -1,11 +1,27 @@
 window.onload = function() {
-    var squareArea = document.getElementById("squarearea");
+    var add = document.getElementById("add");
+    add.onclick = addSquare;
     var squareCount = parseInt(Math.random() * 21) + 30;
-    for (var i = 0; i < squareCount; i++) {
-    var newSquare = document.createElement("div");
+    for (let i = 0; i < squareCount; i++) {
+        addSquare();
+    }
+}
+
+function getRandomColor() {
+    var possibiities = "0123456789ABCDEF";
+    var result = "#";
+    for (var i = 0; i < 6; i++) {
+        result += possibiities.charAt(Math.floor(Math.random() * 16));
+    }
+    return result;
+}
+
+function addSquare() {
+     var squareArea = document.getElementById("squarearea");
+      var newSquare = document.createElement("div");
     newSquare.className = "square";
     newSquare.style.left= parseInt(Math.random()*651) + "px";
     newSquare.style.top= parseInt(Math.random()*251) + "px";
+    newSquare.style.backgroundColor = getRandomColor();
     squareArea.appendChild(newSquare);
-}
 }
